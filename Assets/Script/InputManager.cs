96981030f9591db80edf6c7f44f370a4cb714cfa
Chen-Oku <chenoku@gmail.com>
2025-09-16 15:55:31 -0500

    Player Running WIP

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -4,17 +4,20 @@ public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
     AnimatorManager animatorManager;
+    PlayerMovement playerMovement;
 
     public Vector2 movementInput;
 
     public float verticalInput;
     public float horizontalInput;
+    public float moveAmount;
 
-    private float moveAmount;
+    public bool shiftInput;
 
     private void Awake()
     {
         animatorManager = GetComponent<AnimatorManager>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     private void OnEnable()
@@ -23,6 +26,9 @@ public class InputManager : MonoBehaviour
         {
             playerControls = new PlayerControls();
             playerControls.PlayerMovement.HorizontalMovement.performed += m => movementInput = m.ReadValue<Vector2>();
+
+            playerControls.PlayerActions.Shift.performed += s => shiftInput = true;
+            playerControls.PlayerActions.Shift.canceled += s => shiftInput = false;
 
         }
         playerControls.Enable();
@@ -40,14 +46,23 @@ public class InputManager : MonoBehaviour
 
         moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
 
-        animatorManager.UpdateAnimatorValue(0, moveAmount);
+        animatorManager.UpdateAnimatorValue(0, moveAmount, playerMovement.isRunning);
     }
 
     public void HandleAllInputs()
     {
         HandleMovementInput();
+        HandleRunningInput();
         //JumpInput();
         //Any other inputs can be added here
+    }
+
+    private void HandleRunningInput()
+    {
+        if (shiftInput && moveAmount > 0.5f)
+            playerMovement.isRunning = true;
+        else
+            playerMovement.isRunning = false;
     }
 
 }
