62f8010058ca5e364455e6000076c7fd1654aa50
Chen-Oku <chenoku@gmail.com>
2025-09-18 16:10:52 -0500

    Animaciones Caida y landing

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -1,25 +1,35 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     InputManager inputManager;
     PlayerMovement playerMovement;
+    Animator animator;
+
+    public bool isInteracting;
 
     private void Awake()
     {
         inputManager = GetComponent<InputManager>();
         playerMovement = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
+
     }
 
     void Update()
     {
         inputManager.HandleAllInputs();
-
     }
 
     private void FixedUpdate() //tiene pasos definidos que no dependen de los fps
     {
         playerMovement.HandleAllMovement();
+    }
+
+    private void LateUpdate() //se ejecuta al final de todo
+    {
+        isInteracting = animator.GetBool("isInteracting");
     }
 
 }
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,17 +3,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     InputManager inputManager;
+    PlayerManager playerManager;
     Vector3 moveDirection;
     Transform cameraObject;
     Rigidbody playerRigidbody;
-    public float movementSpeed = 8f;
+    AnimatorManager animatorManager;
+
+    public float inAirTime;
+    public float leapingVelocity;
+    public float fallingVelocity;
+    public float rayCastHeightOffset = 0.5f;
+    public LayerMask groundLayer;
+    public float maxDistance = 1f;
+    public bool isGrounded;
+
+    public float walkingSpeed = 2.5f;
+    public float runningSpeed = 5.0f;
     public float rotationSpeed = 15f;
+    public bool isRunning;
 
 
     private void Awake()
     {
         inputManager = GetComponent<InputManager>();
         playerRigidbody = GetComponent<Rigidbody>();
+        playerManager = GetComponent<PlayerManager>();
+        animatorManager = GetComponent<AnimatorManager>();
         cameraObject = Camera.main.transform;
     }
 
@@ -26,7 +41,14 @@ public class PlayerMovement : MonoBehaviour
 
         moveDirection.Normalize();
 
-        moveDirection *= movementSpeed;
+        if (isRunning)
+
+            moveDirection *= runningSpeed;
+        else
+            moveDirection *= walkingSpeed;
+
+
+
         Vector3 movementVelocity = moveDirection; // hace que el movimiento sea mas fluido
         playerRigidbody.linearVelocity = movementVelocity;
 
@@ -36,11 +58,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 targetDirection = Vector3.zero;
 
-            targetDirection = cameraObject.forward * inputManager.verticalInput;
-            targetDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
-            targetDirection.y = 0;
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.y = 0;
 
-            targetDirection.Normalize();
+        targetDirection.Normalize();
 
         if (targetDirection == Vector3.zero)
             targetDirection = transform.forward;
@@ -53,7 +75,44 @@ public class PlayerMovement : MonoBehaviour
 
     public void HandleAllMovement()
     {
+        if (playerManager.isInteracting)
+            return;
         HandleMovement();
         HandleRotation();
+    }
+
+    public void HandleFallingAndLanding()
+    {
+
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        rayCastOrigin.y += rayCastHeightOffset;
+
+        if (!isGrounded)
+        {
+            if (!playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Falling", true);
+            }
+            inAirTime += Time.deltaTime;
+            playerRigidbody.AddForce(transform.forward * leapingVelocity);
+            playerRigidbody.AddForce(Vector3.down * fallingVelocity * inAirTime);
+        }
+
+        if (Physics.SphereCast(rayCastOrigin, 0.1f, Vector3.down, out hit, maxDistance, groundLayer))
+        {
+            if (!isGrounded && playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Landing", true);
+            }
+            inAirTime = 0;
+            isGrounded = true;
+            playerManager.isInteracting = false;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
     }
 }
